c26cbeaa134148c5f04894ea86740a98dfc2dc2d
krishnasabbu <krishna.sabbu@gmail.com>
2025-09-22 18:56:38 +0530

    Fix Parentheses Syntax Error in ABTestList

--- src/components/Kogito/ABTestList.tsx
+++ src/components/Kogito/ABTestList.tsx
@@ -218,7 +218,7 @@ export default function ABTestList() {
           )}
         </div>
       ) : (
-        (viewMode === 'card' ? (
+        viewMode === 'card' ? (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4">
               {filteredABTests.map((test) => (
                 <div
@@ -453,8 +453,7 @@ export default function ABTestList() {
                 </table>
               </div>
             </div>
-          )}
-        ))
+          )
       )}
     </div>
   );
